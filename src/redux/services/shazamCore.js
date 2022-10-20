211674e221d079d6cb5e65bacb84b23e7c9bcba9
evencoding <evencoding@gmail.com>
2022-10-20 19:07:34 +0900

    Example to using Swiper

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,22 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '716ee5480emsh072d2c35e1a7590p1a7bb7jsn4c7313768fa5',
-    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
-  },
-};
-
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set(
-        'X-RapidAPI-Key',
-        '716ee5480emsh072d2c35e1a7590p1a7bb7jsn4c7313768fa5'
-      );
+      headers.set('X-RapidAPI-Key', process.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
       return headers;
     },
   }),
