9a94aa8dfe9c2566d482747d1b685b7a3d813cf4
evencoding <evencoding@gmail.com>
2022-10-20 18:14:37 +0900

    Example to using Swiper

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,3 @@
+const Searchbar = () => <div />;
+
+export default Searchbar;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '716ee5480emsh072d2c35e1a7590p1a7bb7jsn4c7313768fa5',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+  },
+};
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '716ee5480emsh072d2c35e1a7590p1a7bb7jsn4c7313768fa5'
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
